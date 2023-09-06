15b11c24a16abd1d6fbcf66dfc04a38e57cff8c7
zerosicx <hcsofteng@gmail.com>
2023-09-07 11:51:02 +1200

    Removed bootstrap, added React Bootstrap, added NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,18 @@ import About from './pages/About';
 import Jobs from './pages/Jobs';
 import Hackathons from './pages/Hackathons';
 import Scholarships from './pages/Scholarships';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
    <BrowserRouter>
+   <NavBar></NavBar>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="jobs" element={<Jobs />} />
-          <Route path="hackathon" element={<Hackathons />} />
-          <Route path="scholarships" element={<Scholarships />} />
-          <Route path="*" element={<Home />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/hackathons" element={<Hackathons />} />
+        <Route path="/scholarships" element={<Scholarships />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 type Props = {}
 
 const NavBar = (props: Props) => {
+    
   return (
-    <div>NavBar</div>
+    <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="jobs">Jobs</Nav.Link>
+            <Nav.Link href="hackathons">Hackathons</Nav.Link>
+            <Nav.Link href="scholarships">Scholarships</Nav.Link>
+
+
+          </Nav>
+        </Container>
+      </Navbar>
+
   )
 }
 
