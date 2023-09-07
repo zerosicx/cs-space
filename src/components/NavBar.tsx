6a996652af28853076a30dcd7a349ecd17699673
zerosicx <hcsofteng@gmail.com>
2023-09-07 12:07:01 +1200

    Implemented navbar with simple bootstrap

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,34 @@
-import React, { useState } from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap';
-
+import React from 'react'
 type Props = {}
 
 const NavBar = (props: Props) => {
-    
   return (
-    <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="about">About</Nav.Link>
-            <Nav.Link href="jobs">Jobs</Nav.Link>
-            <Nav.Link href="hackathons">Hackathons</Nav.Link>
-            <Nav.Link href="scholarships">Scholarships</Nav.Link>
-
-
-          </Nav>
-        </Container>
-      </Navbar>
-
+     <nav className="navbar navbar-expand-sm navbar-light bg-light px-3">
+        <a className="navbar-brand" href="/">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+        </button>
+            <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav ms-auto'>
+                <li className='nav-item active'>
+                    <a className='nav-link' href='/'>Home</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='nav-link' href='/about'>About</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='nav-link' href='/jobs'>Jobs</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='nav-link' href='/hackathons'>Hackathons</a>
+                </li>
+                <li className='nav-item'>
+                    <a className='nav-link' href='/scholarships'>Scholarships</a>
+                </li>
+            </ul>
+        </div>        
+      </nav>
   )
 }
 
-export default NavBar;
+export default NavBar
